005682fee7d5c6a0c7cf793496422cf6df079351
Damian Duraczyński <62798664+DamiEn83x@users.noreply.github.com>
2020-10-09 19:55:27 +0200

    another good test

--- src/testing.test.js
+++ src/testing.test.js
@@ -1,5 +1,14 @@
 describe("Tetst Waluty Node", () => {
-  it("Test crash test", () => {
-    expect(1).toEqual(1);
+  var lWalutyService = undefined;
+  beforeAll(() => {
+    var WalutyService = require("./WalutyService/waluty.service.js");
+    lWalutyService = new WalutyService();
+  });
+  it("Test GettabelaWalutAB", (done) => {
+    const CallbackFunction = (output) => {
+      expect(1).toBe(1);
+      done();
+    };
+    lWalutyService.GettabelaWalutAB(CallbackFunction);
   });
 });
